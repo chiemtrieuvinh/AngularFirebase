8c384bd916cb146438c5672c09169a8c42f4a77e
Vinh Chiem <trieuvinb@gmail.com>
2024-06-19 15:45:37 +0700

    update icon

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,11 +4,12 @@ import { RouterLink } from '@angular/router';
 import { AuthenticationService } from '../../services/AuthService/authentication.service';
 import { Observable } from 'rxjs';
 import { CurrentUser } from '../../interfaces/user';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
